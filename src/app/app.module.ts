527d0084f27623017623628916c3602af83a3df7
Bijin Chakraborty <14847418+BijinC@users.noreply.github.com>
2018-07-26 14:51:03 +0400

    Added Angular Material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule, MatExpansionModule, MatCardModule, MatInputModule, MatSnackBarModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,19 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatInputModule,
+    MatSnackBarModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatInputModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
