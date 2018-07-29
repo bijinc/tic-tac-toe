2f48fea6a3cee40329b1e526764de348a73058c2
Bijin Chakraborty <14847418+BijinC@users.noreply.github.com>
2018-07-29 15:40:33 +0400

    Add player turn indicator

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -6,20 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-
+  player1 = true;
+  player2 = false;
   playerTurn = 1;
   gameOver = false;
+
   constructor() { }
 
   ngOnInit() { }
 
   move() {
-    if (this.playerTurn === 1) {
-      this.playerTurn = 2;
-    }
-    else if (this.playerTurn === 2) {
-      this.playerTurn = 1;
-    }
+    this.player1 = !this.player1;
+    this.player2 = !this.player2;
+    // if (this.playerTurn === 1) {
+    //   this.playerTurn = 2;
+    // }
+    // else if (this.playerTurn === 2) {
+    //   this.playerTurn = 1;
+    // }
     this.gameWon();
   }
 
