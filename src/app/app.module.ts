5e981584d3db5642abd73bdeee565b759e4e84f9
Bijin Chakraborty <14847418+BijinC@users.noreply.github.com>
2019-06-18 16:50:17 +0400

    Create Leaderboard component to display top scores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { MenuComponent } from './menu/menu.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
-    MenuComponent
+    MenuComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule,
